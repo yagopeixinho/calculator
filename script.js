c12e75cac93bb1b932afdedb8bf306a7418ecae1
Yago Peixinho <yagopeixinho03@gmail.com>
2021-05-22 17:57:16 -0300

    v0.3

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ function calcular (tipo, valor) {
             document.getElementById('resultado').value = ''  
         }
 
-        if( valor === '+') {
+        if( valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+
+        if ( valor === '=') {
+            var valor_campo = eval(document.getElementById('resultado').value)
             
+            document.getElementById('resultado').value = valor_campo
         }
 
 } else if (tipo === 'valor') {
